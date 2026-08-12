d781fc1e2c8178fd885b79cdbee449a627e1870d
kranthi-kumar <vasamkranthikumar507@gmail.com>
2026-08-12 23:16:33 +0530

    synd

--- backend/hub-ebterprice.ltd/src/main/java/com/hub_entrprice/ltd/controller/AuthController.java
+++ backend/hub-ebterprice.ltd/src/main/java/com/hub_entrprice/ltd/controller/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +19,5 @@ public class AuthController {
         String result = userService.createUser(user);
         return ResponseEntity.ok(result);
     }
-    @GetMapping
-    public Res
 }
 
